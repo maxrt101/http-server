cccb4ee65b4a080b27204b17abdbd28215d7d2ce
Maxym Tkachuk <s2m4f6@gmail.com>
2021-09-21 02:08:04 +0300

    Clean up config struct

--- src/server/config/server_config.h
+++ src/server/config/server_config.h
@@ -2,15 +2,12 @@
 #define SERVER_CONFIG_SERVER_CONFIG_H_
 
 namespace config {
-struct HttpServerCofig {
+struct HttpServerConfig {
   int port = 80;
   int keep_alive_max = 5;
   int keep_alive_timeout = 120;
-  // bool chunked_transfer = false;
   std::string default_doc = "index.html";
   std::string root_folder;
-
-  // void LoadFromConfigFile()
 };
 } // namespace config
 
